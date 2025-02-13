a54588c3ee791cc1e12031c43c17db0e7f9b1399
fauzanocthory <59836825+fauzanocthory@users.noreply.github.com>
2025-02-13 23:55:09 +0700

    Update Userlist.jsx
    ubah url jadi dinamis

--- frontend/src/components/Userlist.jsx
+++ frontend/src/components/Userlist.jsx
@@ -11,12 +11,12 @@ const Userlist = () => {
     }, [])
 
     const getUsers = async () => {
-        const response = await axios.get('http://localhost:5000/users')
+        const response = await axios.get(process.env.REACT_APP_API_URL+'/users')
         setUsers(response.data)
     }
 
     const deleteUser = async (userId) => {
-        await axios.delete(`http://localhost:5000/users/${userId}`)
+        await axios.delete(process.env.REACT_APP_API_URL+'/users/'+userId)
         getUsers()
     }
 
