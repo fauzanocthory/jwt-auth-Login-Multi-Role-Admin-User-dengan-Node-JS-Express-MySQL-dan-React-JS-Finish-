7d8d8759f12fe3a9e17e0ab03f1fa38deb1d6253
fauzanocthory <59836825+fauzanocthory@users.noreply.github.com>
2025-02-13 23:52:35 +0700

    Update FormAddUser.jsx
    ubah url jadi dinamis

--- frontend/src/components/FormAddUser.jsx
+++ frontend/src/components/FormAddUser.jsx
@@ -14,7 +14,7 @@ const FormAddUser = () => {
     const saveUser = async (e) => {
         e.preventDefault()
         try {
-            await axios.post('http://localhost:5000/users', {
+            await axios.post(process.env.REACT_APP_API_URL+'/users', {
                 name: name,
                 email: email,
                 password: password,
