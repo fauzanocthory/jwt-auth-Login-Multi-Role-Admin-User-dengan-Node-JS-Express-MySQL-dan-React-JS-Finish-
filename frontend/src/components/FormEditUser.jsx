3ab785d3c4fd0bb95c5a6f55c1ceaa2d7c7b2c8b
fauzanocthory <59836825+fauzanocthory@users.noreply.github.com>
2025-02-13 23:53:50 +0700

    Update FormEditUser.jsx
    ubah url jadi dinamis

--- frontend/src/components/FormEditUser.jsx
+++ frontend/src/components/FormEditUser.jsx
@@ -15,7 +15,7 @@ const FormEditUser = () => {
     useEffect(()=> {
         const getUserById = async ()=> {
             try {
-                const response = await axios.get(`http://localhost:5000/users/${id}`)
+                const response = await axios.get(process.env.REACT_APP_API_URL+'/users/'+id)
                 setName(response.data.name)
                 setEmail(response.data.email)
                 setRole(response.data.role)
@@ -31,7 +31,7 @@ const FormEditUser = () => {
     const updateUser = async (e) => {
         e.preventDefault()
         try {
-            await axios.patch(`http://localhost:5000/users/${id}`, {
+            await axios.patch(process.env.REACT_APP_API_URL+'/users/'+id, {
                 name: name,
                 email: email,
                 password: password,
